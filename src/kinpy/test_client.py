e6bb47b0fab600b067d571ff4e1384f114eedf64
DanielRubianes <DanielRubianes1352@gmail.com>
2025-02-22 09:50:54 -0500

    More tests

--- src/kinpy/test_client.py
+++ src/kinpy/test_client.py
@@ -5,123 +5,75 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-from interfaces import Kintone, KintoneAuth
+from interfaces import Kintone, KTApp, KintoneAuth
+
+from key import TEST_APP_KEY
+from httpx import Response
+import json
+
+test_auth = KintoneAuth(TEST_APP_KEY)
+
+kintone_portal = Kintone('https://throughthetrees.kintone.com/k/v1/', test_auth, True)
+
+test_app = KTApp(kintone_portal, 13)
+
+get_record = test_app.get_record(id=1)
+
+apps_response: dict = json.loads(get_record().content)
+
+# print(apps_response.headers)
+print(apps_response)
 
 
-test = Kintone('https://throughthetrees.kintone.com/k/v1/', KintoneAuth('132'), True)
-
-print(test.routes.get_app(id=1)())
-
-
-
-print('test')
-sys.exit()
-
-# Get total memory in bytes and convert to GB
-total_memory_gb = psutil.virtual_memory().total / (1024 ** 3)
-
-# Round to the nearest GB
-rounded_memory_gb = round(total_memory_gb)
-
-print(f"Total RAM: {rounded_memory_gb} GB")
-
-import win32com.client
-
-def get_disk_type():
-    wmi = win32com.client.GetObject("winmgmts:")
-    for disk in wmi.InstancesOf("Win32_DiskDrive"):
-        media_type = disk.MediaType  # E.g., "Fixed hard disk media"
-        if "SSD" in disk.Model or "Solid State" in media_type:
-            print(f"Disk: {disk.Model} - SSD")
-        else:
-            print(f"Disk: {disk.Model} - HDD {media_type}")
-
-get_disk_command = 'powershell -NoProfile -ExecutionPolicy Bypass -Command "& { Get-PhysicalDisk | Select-Object DeviceID, MediaType }"'
-
-# Run system command to get serial number
-result = subprocess.run(get_disk_command, capture_output=True, text=True, shell=True)
-
-# Get last "word" from the command output
-# serial_number = result.stdout.strip().split()[-1]
 
 sys.exit()
-# NOW BROKEN ---FIX WITH NEW RECORD DATA STRUCTURE
 
-import tkinter as tk
-from tkinter import messagebox
+from typing import Any
 
 from src.kinpy import App as Kintone_App
+from key import DEVICE_APP_KEY
 
-# Function to show the popup
-def show_popup(text: str=""):
-    messagebox.showinfo("Popup Title", text)
+import sys
+import subprocess
 
-# Create the main application window
-# root = tk.Tk()
-# root.title("Main Window")
-# root.geometry("300x200")
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import messagebox
 
-# # Add a button to trigger the popup
-# button = tk.Button(root, text="Show Popup", command=show_popup)
-# button.pack(pady=20)
+devices_app = Kintone_App(
+    'https://throughthetrees.kintone.com/k/v1/',
+    api_token=DEVICE_APP_KEY,
+    app_id=4
+)
 
-# Run the application
-# root.mainloop()
+serial_command = 'powershell -NoProfile -ExecutionPolicy Bypass -Command "& { Get-CimInstance -ClassName Win32_BIOS | Select-Object SerialNumber }"'
 
-test_app = Kintone_App('https://throughthetrees.kintone.com/k/v1/', api_token='', app_id=13)
+# Run powershell command to get serial number
+result = subprocess.run(serial_command, capture_output=True, text=True, shell=True)
 
+# Get last "word" from the command output
+serial_number = result.stdout.strip().split()[-1]
 
-# devices_app = Kintone_App('https://throughthetrees.kintone.com/k/v1/', api_token='', app_id=4)
-# Fields
-# Drop_down_1: Battery Status
-# Text_7: Model
-# Bay : Bay
-# Drop_down_2: Item Status
-# Text: Asset Tag
+print("Serial Number:", serial_number)
 
-query = ''
+root = tk.Tk()
+root.withdraw()
 
-records: list[ dict[str, dict[str, str] ] ] = test_app.get_records(['Text'], query)
+# "Text" is the internal name of the asset tag field
+# "Text_5" = serial number
+asset_tag = simpledialog.askstring("Kintone Data", "Enter device asset tag:")
+query = f'Text like "%{asset_tag}%"' 
 
+matching_devices: list[ dict[str, Any] ] = devices_app.get_records(['Text', 'Text_5'], query)
 
-# Get records and print info out
-all_fields = []
-asset_tags: list[str] = []
-for record in records:
-    # print('=======================')
-    # print(record)
-    for field, value in record.items():
-        if field not in all_fields:
-            all_fields.append(field)
-        if field == 'Text':
-            asset_tags += value
+if not asset_tag:
+    sys.exit()
 
-# print( sorted(records, key=lambda x: int(x)) )
-print(all_fields)
-print(len(records))
-
-# Update records
-# devices_app.update_records('Bay', 'Needs Parts Shelf', shelf_query)
-
-# Create 500 ton of records
-# duped_rows = [
-#     {
-#         'Record_Status': {'value': 'Need to Order'},
-#         'Text_1': {'value': i},
-#     }
-#     for i in range(501)
-# ]
-# chunk_size = 100
-# for i in range(0, len(duped_rows), 100):
-#     chunk = duped_rows[i:i+100]
-#     payload = {
-#         "app": "13", # API Test App
-#         "records": chunk
-#     }
-#     response: BaseHTTPResponse = http.request(
-#         "POST", "https://throughthetrees.kintone.com/k/v1/records.json",
-#         headers={"X-Cybozu-API-Token": "",
-#             "Content-Type": "application/json"},
-#         body=json.dumps(payload).encode('utf-8')
-#     )
-#     print(response.data)
+if not matching_devices or len(matching_devices) < 1:
+    messagebox.showerror("Info", f"Asset {asset_tag} not found!")
+elif matching_devices[0]['Text_5'] == serial_number:
+    messagebox.showinfo("Info", "This record has the correct serial number.")
+elif devices_app.update_record(query, 'Text_5', serial_number):
+    output = f"Device Serial Number: {serial_number}"
+    output = f"Original Value: {matching_devices[0]['Text_5']}\n" + output if matching_devices[0]['Text_5'] else output
+    messagebox.showinfo("Serial Number Updated", output)
