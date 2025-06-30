4330913d638a5043d86adacec0eae6020a489a7a
DanielRubianes <DanielRubianes1352@gmail.com>
2025-06-30 09:16:49 -0400

    Update 06-30-2025 to square.py

--- src/kinpy/square.py
+++ src/kinpy/square.py
@@ -1,11 +1,15 @@
 import httpx
 
 import datetime
+from datetime import datetime as dt
 import re
+from collections import Counter
+
+from key import DEVICE_AUTH, SQUARE_KEY
 
 headers = {
     "Square-Version": "2025-05-21",
-    "Authorization": "Bearer ",
+    "Authorization": f"Bearer {SQUARE_KEY}",
     "Content-Type": "application/json"
 }
 
@@ -18,7 +22,7 @@ headers = {
 #     print(payment['created_at'])
 #     print(payment['device_details'])
 #     print(payment['application_details'])
-#     time: datetime.datetime = datetime.datetime.fromisoformat(payment['created_at'])
+#     time: dt = dt.fromisoformat(payment['created_at'])
 #     print(f'{time.strftime("%B")} {time.day} @ {time.hour%12}:{time.minute}')
 # response: dict = httpx.get("https://connect.squareup.com/v2/locations", headers=headers).json()
 # locations = response
@@ -34,7 +38,7 @@ body = {
         "filter": {
             "date_time_filter": {
                 "created_at": {
-                "start_at": "2019-01-01T00:00:00Z",
+                "start_at": "2020-01-01T00:00:00Z",
                 "end_at": "2025-06-28T23:59:59Z"
                 }
             }
@@ -46,8 +50,10 @@ search_orders_endpoint = "https://connect.squareup.com/v2/orders/search"
 more_items: bool = True
 cursor = None
 all_notes: str = ''
-earliest_datetime = datetime.datetime.now(datetime.timezone.utc)
-latest_datetime = datetime.datetime.min.replace(tzinfo=datetime.timezone.utc)
+test_dict: dict[dt, list] = {}
+
+earliest_datetime = dt.now(datetime.timezone.utc)
+latest_datetime = dt.min.replace(tzinfo=datetime.timezone.utc)
 while more_items:
     body["cursor"] = cursor
     response: dict = httpx.post(search_orders_endpoint, headers=headers, json=body).json()
@@ -59,7 +65,7 @@ while more_items:
     orders: list[dict] = response['orders']
 
     for order in orders:
-        created_datetime: datetime.datetime = datetime.datetime.fromisoformat(order['created_at'])
+        created_datetime: dt = dt.fromisoformat(order['created_at'])
         if created_datetime < earliest_datetime:
             earliest_datetime = created_datetime
         if created_datetime > latest_datetime:
@@ -67,17 +73,22 @@ while more_items:
 
         if 'line_items' in order:
             items = order['line_items']
+            test_dict[created_datetime] = []
             for item in items:
                 if 'note' in item:
+                    test_dict[created_datetime].extend(re.findall(r"\d{4}", item['note']))
                     all_notes += item['note'] + '~'
-
-sold_assets = re.findall(r"\d{4}", all_notes)
 
 print(f'First transaction: {earliest_datetime.strftime("%B")} {earliest_datetime.day}, {earliest_datetime.year} @ {earliest_datetime.hour%12}:{earliest_datetime.minute}')
 print(f'Last transaction: {latest_datetime.strftime("%B")} {latest_datetime.day}, {latest_datetime.year} @ {latest_datetime.hour%12}:{latest_datetime.minute}')
+sold_assets = re.findall(r"\d{4}", all_notes)
+print(f'{len(sold_assets)=}\n{len([asset for assets in test_dict.values() for asset in assets])=}')
+# print([asset for assets in test_dict.values() for asset in assets])
+# sold_assets = [w for w in re.findall(r'\b\w+\b', all_notes) if len(re.findall(r'\d', w)) == 4]
+# Ignore items returned and sold multiple times / record duplicates
+unique_sold_assets = [asset for asset, _ in Counter(sold_assets).items()]
 
 from interfaces import KintonePortal, KTApp, QueryString
-from key import DEVICE_AUTH
 
 kintone_portal = KintonePortal('https://throughthetrees.kintone.com/k/v1/', DEVICE_AUTH, True)
 devices_app = KTApp(kintone_portal, 4)
@@ -85,25 +96,22 @@ devices_app = KTApp(kintone_portal, 4)
 ASSET_TAG = "Text"
 ITEM_STATUS = "Drop_down_2"
 records: list = []
-for i in range(0, len(sold_assets), 100):
-    chunk = sold_assets[i:i + 100]
-    query = QueryString(f'{ASSET_TAG} in ({', '.join([f'\"{s}\"' for s in chunk])})')
+record_errors: dict[str, list[str]] = {}
+for i in range(0, len(unique_sold_assets), 100):
+    chunk = unique_sold_assets[i:i + 100]
+    query = QueryString(f"{ASSET_TAG} in ({', '.join(str(s) for s in chunk)})")
     records.extend( devices_app.get_records([ASSET_TAG, ITEM_STATUS], query) )
 
 for record in records:
     if record[ITEM_STATUS] != 'Sold':
-        print(f'Asset {record[ASSET_TAG]} marked {record[ITEM_STATUS]}!')
+        if record[ITEM_STATUS] not in record_errors:
+            record_errors[record[ITEM_STATUS]] = []
+        record_errors[record[ITEM_STATUS]].append(record[ASSET_TAG])
+        # print(f'Asset {record[ASSET_TAG]} marked {record[ITEM_STATUS]}!')
 
-print(len(sold_assets))
-print(len(records))
+for k, v in record_errors.items():
+    print(f'{len(v)} records marked as {k}: {v}')
 
 found_assets: list[str] = [record[ASSET_TAG] for record in records]
-
 missing_assets: list[str] = [asset for asset in sold_assets if asset not in found_assets]
-
-print(f'{len(found_assets)} found records: XX{[]}')
-print(f'{len(missing_assets)} missing records: {[missing_assets]}')
-
-from collections import Counter
-duplicates = duplicates = [item for item, count in Counter(sold_assets).items() if count > 1]
-print(f'{len(duplicates)} duplicates sold: {[duplicates]}')
+print(f'{len(missing_assets)} missing records: {missing_assets}')
